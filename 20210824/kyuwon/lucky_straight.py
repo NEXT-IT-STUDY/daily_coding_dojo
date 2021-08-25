7fb5e1613aeeb19bbee299fa3cd804844bffde62
kyuwon lee <86951640+Kyuwon53@users.noreply.github.com>
2021-08-25 13:22:09 +0900

    [UPDATE] 변수 추가

--- 20210824/kyuwon/lucky_straight.py
+++ 20210824/kyuwon/lucky_straight.py
@@ -12,10 +12,10 @@
 n = input()
 left = 0
 right = 0
-
-for i in range(len(n)//2):
+length = len(n)
+for i in range(length//2):
     left += int(n[i])
-for i in range(len(n)//2, len(n)):
+for i in range(length//2, length):
     right += int(n[i])
 
 
