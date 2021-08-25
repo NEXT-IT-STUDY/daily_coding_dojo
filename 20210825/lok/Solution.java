a3c4c72a9121e2ff64dc72d5d4a107ddcef3d37e
lok <milkteatea0628@gmail.com>
2021-08-25 23:49:49 +0900

    부족한 금액 계산하기

---
+++ 20210825/lok/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        
+        //여기서부터 풀이
+        int sum = 0;
+        for(int i=1; i<=count; i++){
+            sum += price*i; 
+            System.out.print("sum:"+sum);
+        }
+        if(money-sum >= 0){
+            answer = 0;
+        }else{
+            answer = answer*(money-sum);
+        }
+        
+        return answer;
+    }
+}
